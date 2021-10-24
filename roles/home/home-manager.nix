3324f4e3a49515dd51ebdc9e7d55254c06e300e8
Sandro Stikić <sandro@stikic.com>
2021-10-24 15:17:27 +0800

    Add ssh config

--- roles/home/home-manager.nix
+++ roles/home/home-manager.nix
@@ -1,5 +1,23 @@
-{ ... }: {
+{ config, ... }: {
   programs.git = {
     userEmail = "sandro@stikic.com";
+  };
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        hostname = "github.com";
+        identityFile = "${config.home.homeDirectory}/.ssh/keys/github.com";
+      };
+      "stikic.me" = {
+        hostname = "stikic.me";
+        identityFile = "${config.home.homeDirectory}/.ssh/keys/stikic.me";
+      };
+      "stikic.com" = {
+        hostname = "stikic.com";
+        identityFile = "${config.home.homeDirectory}/.ssh/keys/stikic.com";
+      };
+    };
   };
 }
