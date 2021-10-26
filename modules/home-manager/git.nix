b00e496c067f0faaddab85295024823f37e8fb14
Sandro Stikić <sandro@stikic.com>
2021-10-26 18:14:19 +0800

    Update git config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -4,7 +4,18 @@
     userName = "Sandro Stikić";
     ignores = [ ".DS_Store" ];
     extraConfig = {
+      core = {
+        autocrlf = false;
+        editor = "code --wait";
+        eol = "lf";
+      };
+      diff.tool = "vscode";
+      difftool.vscode.cmd = "code --wait --diff $LOCAL $REMOTE";
       init.defaultBranch = "main";
+      merge.tool = "vscode";
+      pull.rebase = true;
+      push.default = "current";
+      rebase.autoStash = true;
     };
   };
 }
