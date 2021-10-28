bc10de39b4c3f37c6a4737c6d67e113a984d86aa
Sandro Stikić <sandro@stikic.com>
2021-10-28 16:22:26 +0800

    Add garbage collection

--- modules/default.nix
+++ modules/default.nix
@@ -1,10 +1,23 @@
 { pkgs, ... }: {
   imports = [ ./user.nix ];
 
-  # Use flakes for **maximum hermeticism**.
   nix = {
+    # Use flakes for **maximum hermeticism**.
     package = pkgs.nixFlakes;
-    extraOptions = "experimental-features = nix-command flakes";
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      # Uses more disk space but speeds up nix-direnv.
+      keep-keep-derivations = true
+      keep-outputs = true
+      # Trigger garbage collection when <= 1GB is free.
+      min-free = ${toString (1000 * 1000 * 1000)}
+    '';
+    # Automatically collect garbage.
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
 
   # Allow proprietary packages.
