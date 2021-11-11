e2f29c2cad04e28691b14d751363318a85cba449
Sandro Stikić <s.stikic@fugro.com>
2021-11-11 15:15:43 +0800

    Fix missing boot drive

--- hosts/marisa/hardware.nix
+++ hosts/marisa/hardware.nix
@@ -32,6 +32,10 @@
       device = "/dev/disk/by-label/root";
       fsType = "ext4";
     };
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
+      fsType = "vfat";
+    };
   };
 
   # Encrypted drives.
